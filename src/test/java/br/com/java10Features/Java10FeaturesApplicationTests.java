a3a305e2f122f5181ef17d59013d0a17cec0079f
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-06 22:21:34 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/java10Features/Java10FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java10Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java10FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
